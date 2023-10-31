61eaab553b5511393f7aa0be1c44f4dd2a0271e4
diogocerutti <diogo.cerutti19@gmail.com>
2023-10-31 10:48:00 -0300

    Novo diretório imagens front

--- utils/upload.js
+++ utils/upload.js
@@ -2,7 +2,7 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "../new-tcc-front/src/img");
+    cb(null, "../Produtos");
   },
   filename: function (req, file, cb) {
     cb(null, new Date().valueOf() + "_" + file.originalname);
