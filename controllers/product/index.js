0cbac22468412f614ca0f7d4f6ebd52a1f8ec34a
diogocerutti <diogo.cerutti19@gmail.com>
2023-11-04 00:36:09 -0300

    Removido where do getAllProducts

--- controllers/product/index.js
+++ controllers/product/index.js
@@ -8,7 +8,7 @@ import { validationResult } from "express-validator";
 export const getAllProducts = async (req, res) => {
   try {
     const products =
-      await db.$queryRaw`SELECT product.id, name, price, description, image, category, measure, status FROM product INNER JOIN product_category ON product.id_category=product_category.id INNER JOIN measure_type ON product.id_measure=measure_type.id WHERE product.status = true`;
+      await db.$queryRaw`SELECT product.id, name, price, description, image, id_category, id_measure, category, measure, status FROM product INNER JOIN product_category ON product.id_category=product_category.id INNER JOIN measure_type ON product.id_measure=measure_type.id`;
 
     Object.keys(products).forEach((item) => {
       for (const key in products[item]) {
